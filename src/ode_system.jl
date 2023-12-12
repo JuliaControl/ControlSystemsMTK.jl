411c182c00c68325627cda2c556c9b483e9ee2e0
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-12-12 15:08:20 +0100

    Update ode_system.jl

--- src/ode_system.jl
+++ src/ode_system.jl
@@ -633,10 +633,17 @@ y        &= C(v) x + D(v) u
 
 where `v` is a scalar scheduling variable.
 
+See example usage in the [gain-scheduling example](https://juliacontrol.github.io/ControlSystemsMTK.jl/dev/batch_linearization/#Gain-scheduling).
+
 # Arguments:
 - `systems`: A vector of `ControlSystemsBase.StateSpace` objects
 - `vt`: A vector of breakpoint values for the scheduling variable `v`, this has the same length as `systems`.
 - `interpolator`: A constructor `i = interpolator(values, breakpoints)` and returns an interpolator object that can be called like `i(v)` to get the interpolated value at `v`. `LinearInterpolation` from DataInterpolations.jl is a good choice, but a lookup table can also be used.
+
+# Connectors
+- `input` of type `RealInput` connects to ``u``.
+- `output` of type `RealOutput` connects to ``y``.
+- `scheduling_input` of type `RealInput` connects to ``v``.
 """
 function GainScheduledStateSpace(systems, vt; interpolator, x = zeros(systems[1].nx), name, u0 = zeros(systems[1].nu), y0 = zeros(systems[1].ny))
 
